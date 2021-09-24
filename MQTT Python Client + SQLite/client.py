b74adf51017a3c518b423c44053147985c283111
jordaycs <jordaycs@gmail.com>
2021-09-24 19:40:00 +0200

    refactor client

--- MQTT Python Client + SQLite/client.py
+++ MQTT Python Client + SQLite/client.py
@@ -1,6 +1,11 @@
 import paho.mqtt.client as mqttClient
 from transformer import transform
 
+BROKER_ENDP = "192.168.1.100"
+BROKER_PORT = 1883
+BROKER_USER = "hackday"
+BROKER_PASS = "hadkday"
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected")
@@ -21,16 +26,13 @@ def on_message(client, userdata, message):
 
 broker_address = "192.168.1.100"    # Broker address
 port = 1883                         # Broker port
-user = "hackday"                  # Connection username
-password = "hackday"              # Connection password
+user = "hackday"                    # Connection username
+password = "hackday"                # Connection password
 
 
-client = mqttClient.Client("jonas_python_mqtt")          # create new instance
-client.username_pw_set(user, password=password)     # set username and password
-# attach function to callback
+client = mqttClient.Client("jonas_python_mqtt")
+client.username_pw_set(BROKER_USER, password=BROKER_PASS)
 client.on_connect = on_connect
-# attach function to callback
 client.on_message = on_message
-client.connect(broker_address, port, 60)            # connect
-# subscribe
-client.loop_forever()                               # then keep listening forever
+client.connect(BROKER_ENDP, BROKER_PORT, 60)
+client.loop_forever()
